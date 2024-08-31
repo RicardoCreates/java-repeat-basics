67a6d5e7d0d6ee858f7d3bdbd013e7f60399ca22
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-08-31 16:44:47 +0200

    Basic Operators

--- src/main/java/de/ricardo/Main.java
+++ src/main/java/de/ricardo/Main.java
@@ -10,7 +10,32 @@ public class Main {
         boolean isValid = true;
         char letter = 'c';
 
-        
+        //example of variables with different data types
+        String name = "John Doe";
+
+        //Beispiel für die Verwendung von Operatoren
+        int x = 10;
+        int y = 5;
+
+        int sum = x + y;
+        System.out.println(sum);
+        int difference = x - y;
+        System.out.println(difference);
+        int product = x * y;
+        System.out.println(product);
+        int quotient = x / y;
+        System.out.println(quotient);
+
+        boolean isEqual = x == y;
+        System.out.println(isEqual);
+        boolean isGreater = x > y;
+        System.out.println(isGreater);
+        boolean logicalAnd = x > 0 && y >0;
+        System.out.println(logicalAnd);
+        boolean logicalOr = x > 0 || y > 0;
+        System.out.println(logicalOr);
+        boolean logicalNot = !(x > y);
+        System.out.println(logicalNot);
 
 
     }
