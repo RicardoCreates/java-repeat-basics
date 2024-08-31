92f8c1158dd791fd95ca1cc1eca331388b92cb0a
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-08-31 16:28:53 +0200

    Initial commit

---
+++ src/main/java/de/ricardo/Main.java
@@ -0,0 +1,17 @@
+package de.ricardo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //example of declaring a variable
+        int age = 35;
+        double pi = 3.14;
+        boolean isValid = true;
+        char letter = 'c';
+
+        
+
+
+    }
+}
